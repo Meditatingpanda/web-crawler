8677c84f3bcc3e2dfc96ed111dc2bdb06f384023
Gyana Ranjan panda <grp.gyanaranjan@gmail.com>
2022-07-21 07:06:56 +0530

    make it better

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 const puppeteer = require('puppeteer');
 const bot = async (url) => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+        headless: true, 'args': [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    });
     const page = await browser.newPage();
     await page.setRequestInterception(true);
- //if the page makes a  request to a resource type of image or stylesheet then abort that request
+    //if the page makes a  request to a resource type of image or stylesheet then abort that request
     page.on('request', request => {
         if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || request.resourceType() === 'font' || request.resourceType() === 'manifest' || request.resourceType() === 'script' || request.resourceType() === 'texttrack' || request.resourceType() === 'fetch' || request.resourceType() === 'eventsource' || request.resourceType() === 'websocket' || request.resourceType() === 'xhr' || request.resourceType() === 'other')
             request.abort();
@@ -17,7 +22,7 @@ const bot = async (url) => {
         return document.querySelector('.code-container').innerHTML;
     })
     console.log('scrapping done!!')
-    
+
     await page.close();
     await browser.close();
     return grabCode;
