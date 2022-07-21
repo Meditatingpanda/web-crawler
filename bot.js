a0fae91dfa05bde891ad9166e05fb18b12542093
Gyana Ranjan panda <grp.gyanaranjan@gmail.com>
2022-07-21 13:42:54 +0530

    scraping all data

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer');
 const bot = async (url) => {
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
+    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.setRequestInterception(true);
+
     //if the page makes a  request to a resource type of image or stylesheet then abort that request
     page.on('request', request => {
         if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || request.resourceType() === 'font' || request.resourceType() === 'manifest' || request.resourceType() === 'script' || request.resourceType() === 'texttrack' || request.resourceType() === 'fetch' || request.resourceType() === 'eventsource' || request.resourceType() === 'websocket' || request.resourceType() === 'xhr' || request.resourceType() === 'other')
@@ -14,7 +15,14 @@ const bot = async (url) => {
     await page.goto(url);
 
     const grabCode = await page.evaluate(() => {
-        return document.querySelector('.code-container').innerHTML;
+        const allTags = document.querySelectorAll('.code-container');
+        const lan = document.querySelectorAll('.tabtitle')
+        let data = []
+        allTags.forEach((ele, i) => {
+            data.push({ code: ele.innerText, lan: lan[i].innerText })
+        })
+
+        return data;
     })
     console.log('scrapping done!!')
 
